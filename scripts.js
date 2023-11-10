eb8c1a03981a20639b4df6082a12f4beb0acf283
tLORDt <101082389+tLORDt@users.noreply.github.com>
2023-11-10 21:02:59 +1300

    Idek tbh

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+let rolls = 0;
+
 function rollDice() {
     // Get a random number between 1 and 6
     var randomNumber = Math.floor(Math.random() * 6) + 1;
@@ -7,19 +9,27 @@ function rollDice() {
 }
 
 function getDice(nDice) {
-    // Get dice values
-    let numberStr = " " + rollDice()
 
-    for(let i = 1; i < nDice; i++) {
-      numberStr += " " + rollDice();
+    // check if first roll
+    if (rolls < 1){
+        rolls++; 
+         // Get dice values
+        let numberStr = " " + rollDice()
+
+        for(let i = 1; i < nDice; i++) {
+            numberStr += " " + rollDice();
+        }
+
+        // Display Dice Rolls to User
+        document.getElementById("dice").innerText = "Your Dice:" + numberStr
+    } else {
+        console.log("Roll failed: only can roll once a game.")
     }
-
-    // Display Dice Rolls to User
-    document.getElementById("dice").innerText = "Your Dice:" + numberStr
 }
 
 function makeBet(){
-    let message = document.getElementById("message").value;
+    let numberDice = document.getElementById("numberDice").value;
+    let valueDice = document.getElementById("valueDice").value;
 
-    console.log(message);
+    console.log("Bet that there are " + numberDice + " di with value " + valueDice);
 }
